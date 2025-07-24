23477513c36bca923adfe2c7792f633950917e4f
ojo19001@byui.edu <aji22001@byui.edu>
2025-07-24 14:47:21 +0100

    Seller View

---
+++ my-app/src/app/seller/page.tsx
@@ -0,0 +1,20 @@
+
+import styles from "../page.module.css";
+ import Navbar from "../ui/navbars";
+ import Footer from "../ui/footer";
+ import Production from "../ui/products"
+
+export default function products() {
+  return (
+    <div className={styles.page}>
+
+    <Navbar />
+
+    <div>
+        Coming Soon!
+    </div>
+
+    <Footer />
+    </div>
+  );
+}
--- my-app/src/app/ui/navbars.jsx
+++ my-app/src/app/ui/navbars.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Link from "next/link";
 import styles from "../page.module.css";
 
-
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
@@ -11,18 +10,25 @@ export default function Navbar() {
   return (
     <>
       <nav className={styles.nav}>
-
         <div className={styles.hambuger} onClick={toggleMenu}>
           <div className={styles.bar1}></div>
           <div className={styles.bar2}></div>
           <div className={styles.bar3}></div>
         </div>
-        
+
         <ul className={`${styles.navLinks} ${isOpen ? styles.showMenu : ""}`}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">Seller</Link></li>
-          <li><Link href="/products">Products</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/seller">Seller</Link>
+          </li>
+          <li>
+            <Link href="/products">Products</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
         </ul>
       </nav>
     </>
